7aa42f442a97e6879c80b443f0558ad965e032ee
Mathieu Chauveau <chauveau.math@gmail.com>
2024-01-17 21:19:08 +0100

    bug scss à fixer

--- js/frontend/src/pages/Points.jsx
+++ js/frontend/src/pages/Points.jsx
@@ -31,45 +31,116 @@ function Points() {
     {
       id: 2,
       name: "rouge",
-      img: "./src/assets/lorealproduct.jpg",
+      img: "./src/assets/correcteur.jpg",
       price: "10euros",
     },
     {
       id: 3,
       name: "creme",
+      img: "./src/assets/creme-main.jpg",
+      price: "30euros",
+    },
+    {
+      id: 4,
+      name: "pipe",
+      img: "./src/assets/lorealproduct.jpg",
+      price: "50euros",
+    },
+    {
+      id: 5,
+      name: "creamy",
       img: "./src/assets/lorealproduct.jpg",
       price: "30euros",
     },
   ];
 
+  const shops = [
+    {
+      id: 1,
+      name: "Salon de coiffure Hitl'hair",
+      img: "./src/assets/coiffure.jpeg",
+    },
+    {
+      id: 2,
+      name: "Salon de manucure ongle ben",
+      img: "./src/assets/manucure.jpeg",
+    },
+    {
+      id: 3,
+      name: "Pharma Syrie",
+      img: "./src/assets/pharmacie.jpeg",
+    },
+    {
+      id: 4,
+      name: "Aero Gros porc",
+      img: "./src/assets/lorealproduct.jpg",
+    },
+    {
+      id: 5,
+      name: "Parfumeriz basmati",
+      img: "./src/assets/lorealproduct.jpg",
+    },
+  ];
+
   return (
     <div className="point-container">
-      <div className="point-part">
-        <h1>Mes points</h1>
-        <h2 className="points">100 points</h2>
+      <div className="point-part container-max t-center bg-black">
+        <div className="contain">
+          <div className="d-flex d-flex-center">
+            <div>
+              <h1 className="mb-20">Mes points</h1>
+              <h2 className="bg-white radius-10 mb-20 points">100 points</h2>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="favorite-part">
-        <h2>Mes produits favoris</h2>
-        <div className="line-product">
-          {products.map((data) => (
-            <div key={data.id}>
+      <div className="favorite-part bg-white border-10">
+        <div className="contain">
+          <h2>Mes produits favoris</h2>
+          <div className="horizontal-scroll has-little-cards line-product">
+            {products.map((data) => (
+              <div
+                className="card product-card d-flex d-flex-space-between d-flex-column bg-black radius-10"
+                key={data.id}
+              >
+                <img src={data.img} alt="product" />
+                <div>
+                  <h4>{data.name}</h4>
+                  <h4>{data.price}</h4>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="shop bg-black contain">
+        <h2>Mes points de ventes locaux</h2>
+        <div className="horizontal-scroll has-big-cards line-product mt-20 mb-20">
+          {shops.map((data) => (
+            <div
+              className="card shop-card d-flex d-flex-space-between d-flex-column bg-white radius-10"
+              key={data.id}
+            >
               <img src={data.img} alt="product" />
-              <h4>{data.name}</h4>
-              <h4>{data.price}</h4>
+              <div>
+                <h4>{data.name}</h4>
+              </div>
             </div>
           ))}
         </div>
       </div>
-      <div className="shop">
-        <h2>Mes points de ventes locaux</h2>
-      </div>
-      <div className="reduc">
+      <div className="bg-white reduc mb-30">
         <h2>Mes offres</h2>
-        <div className="line-offer">
+        <div className="horizontal-scroll line-offer">
           <img src="./src/assets/reduc.png" alt="reduc" />
           <h3>Pour 100 Points</h3>
         </div>
-        <button type="button">Générer mon code</button>
+        <button
+          className="mi-auto radius-10 pb-10 bg-black mt-30"
+          type="button"
+        >
+          Générer mon code
+        </button>
       </div>
     </div>
   );
