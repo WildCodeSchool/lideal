e2c97ceebf38689164f9319b1c07bda2607d1313
Mathieu Chauveau <chauveau.math@gmail.com>
2024-01-18 12:07:04 +0100

    modif picture avec ajout bouton

--- js/frontend/src/pages/Picture.jsx
+++ js/frontend/src/pages/Picture.jsx
@@ -3,10 +3,44 @@ import "./picture.scss";
 import iconePhoto from "../assets/iconePictureBlack.svg";
 
 function Picture() {
+  const handleOpenCamera = () => {
+    // ouvre la caméra en demandant les droits d'accès au navigateur
+    if (!cameraPopup || !stream) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((newStream) => {
+          setStream(newStream);
+
+          if (videoRef.current) {
+            // réinitialise le flux vidéo
+            videoRef.current.srcObject = null;
+            videoRef.current.srcObject = newStream;
+          }
+        })
+        .catch((error) => {
+          console.error(
+            "Erreur lors de l'ouverture de l'appareil photo :",
+            error
+          );
+        });
+    }
+  };
+  const handleCloseCamera = () => {
+    // coupe la camera
+    if (stream) {
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+      setStream(null);
+    }
+  };
   return (
     <div className="picture-container bg-black">
       <div className="picture">
-        <h1>yo</h1>
+        <div className="btn-autorisation">
+          <button type="button" onClick={handleOpenCamera}>
+            Ouvrir camera
+          </button>
+        </div>
       </div>
       <div className="logo-picture">
         <img src={iconePhoto} alt="iconePhoto" />
